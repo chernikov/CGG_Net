c78df454ac1025c21457e890bf4fdbc68ea27023
Andriy Chernikov <chernikov@gmail.com>
2026-02-08 14:43:59 +0200

    chore: add docker deployment

--- src/CGG.Api/Program.cs
+++ src/CGG.Api/Program.cs
@@ -26,8 +26,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
